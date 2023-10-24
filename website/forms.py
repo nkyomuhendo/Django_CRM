dd7401209e27eb8b54b55ca4f09c50789da2f5ec
nkyomuhendo <nkyomuhendo@gmail.com>
2023-10-24 19:18:59 +0300

    Corrected errors in forms module of attr and also concluded the register page

--- website/forms.py
+++ website/forms.py
@@ -3,9 +3,9 @@ from django.contrib.auth.models import User
 from django import forms
 
 class SignUpForm(UserCreationForm):
-    email = forms.EmailField(label="", widget=forms.TextInput(attr={'class':'form-control', 'placeholder':'Email Address'}))
-    first_name = forms.CharField(label="", max_length=100, widget=forms.TextInput(attr={'class':'form-control', 'placeholder':'First Name'}) )
-    last_name = forms.CharField(label="", max_length=100, widget=forms.TextInput(attr={'class':'form-control', 'placeholder':'Last Name'})  )
+    email = forms.EmailField(label="", widget=forms.TextInput(attrs={'class':'form-control', 'placeholder':'Email Address'}))
+    first_name = forms.CharField(label="", max_length=100, widget=forms.TextInput(attrs={'class':'form-control', 'placeholder':'First Name'}) )
+    last_name = forms.CharField(label="", max_length=100, widget=forms.TextInput(attrs={'class':'form-control', 'placeholder':'Last Name'})  )
 
 
     class Meta:
@@ -28,3 +28,4 @@ class SignUpForm(UserCreationForm):
         self.fields['password2'].widget.attrs['class'] = 'form-control'
         self.fields['password2'].widget.attrs['placeholder'] = 'Confirm Password'
         self.fields['password2'].label = ''
+        self.fields['password2'].help_text = '<span class="form-text text-muted"><small>Enter the same password as before, for verification.</small></span>'
